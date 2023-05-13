1d778003d0af1bab5c394fa7c241499af4f40f40
Eduardo <edu.he.dev@gmail.com>
2023-05-13 17:53:45 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import TextEditor from "./components/TextEditor.svelte";
+</script>
+
+<main>
+  <TextEditor></TextEditor>
+</main> 
+
+<style>
+</style>
---
+++ src/components/TextEditor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let text = '';
+  let selectionStart: number = 0;
+  let selectionEnd: number = 0;
+
+  function handleTab(event: KeyboardEvent<HTMLTextAreaElement>) {
+
+    if(event.key === "Tab"){
+      event.preventDefault();
+    const textarea = event.target as HTMLTextAreaElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    text = text.substring(0, start) + '\t' + text.substring(end);
+    textarea.selectionStart = textarea.selectionEnd = start + 1;
+    }
+   
+  }
+</script>
+
+<textarea
+  bind:value={text}
+  on:keydown={handleTab}
+
+/>
+
+<p>Selection start: {selectionStart}</p>
+<p>Selection end: {selectionEnd}</p>
